da0fd53ba887fdb8d03bfaaa3bb3e09665bc08cb
himajavuyyuru <vuyyuruhimaja@gmail.com>
2026-09-07 12:04:34 +0530

    Add files via upload

--- fitness-checker/config.py
+++ fitness-checker/config.py
@@ -1,6 +1,8 @@
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+INSTANCE_DIR = os.path.join(BASE_DIR, "instance")
+os.makedirs(INSTANCE_DIR, exist_ok=True)
 
 
 class Config:
@@ -16,6 +18,6 @@ class Config:
 
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL",
-        "sqlite:///" + os.path.join(BASE_DIR, "instance", "fitness.db"),
+        "sqlite:///" + os.path.join(INSTANCE_DIR, "fitness.db"),
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
